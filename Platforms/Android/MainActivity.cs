bbe551dfa9fd102c272bcb13bee47ab2dc1ef14f
nminjkfftghjxf <147725607+nminjkfftghjxf@users.noreply.github.com>
2025-06-26 18:24:50 +0300

    Add files via upload

---
+++ Platforms/Android/MainActivity.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.InputMethodServices;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+
+namespace plante2._3
+{
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    public class MainActivity : MauiAppCompatActivity
+    {
+       
+    }
+}
